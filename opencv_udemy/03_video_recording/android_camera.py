0c8373381dc99ab664823c31468c7591849f3076
Eda <edacakmak92@gmail.com>
2023-07-09 19:50:06 +0300

    android_camera.py

--- opencv_udemy/03_video_recording/android_camera.py
+++ opencv_udemy/03_video_recording/android_camera.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import requests
 
-url = "http://192.168.1.158:8080//shot.jpg"
+url = "http://192.168.1.158:8080//shot.jpg" #The image url we obtained thanks to the IP webcam application
 
 while True:
     img_resp = requests.get(url)
