6d33a148a77e097503cda7d7d2eb8961d5d9e077
Eda <edacakmak92@gmail.com>
2023-07-09 19:46:58 +0300

    image_reading_showing_saving.py

--- opencv_udemy/01_image_reading_showing_saving/image_reading_showing_saving.py
+++ opencv_udemy/01_image_reading_showing_saving/image_reading_showing_saving.py
@@ -3,8 +3,8 @@ import cv2
 img = cv2.imread("resim1.jpg", 0)
 # print(img)
 cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-cv2.imshow("image", img) #resmi görüntülememizi sağlıyor
+cv2.imshow("image", img) #allows us to view the image
 cv2.imwrite("resim1.jpg", img)
-cv2.waitKey(0) #açılan pencerenin biz kapatana kadar açık kalmasını sağlıyor
+cv2.waitKey(0) #akeeps the popup open until we close it
 cv2.destroyAllWindows()
 
